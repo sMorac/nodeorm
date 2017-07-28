b4e0b2792b3c3eff3d7563683b02f0432f45b10b
sMorac <stephane@moracchini.fr>
2017-07-28 16:41:15 +0100

    Update ES6 / promises + simplications

--- pgmodel.js
+++ pgmodel.js
@@ -1,46 +1,59 @@
 'use strict';
 /*jshint esversion: 6 */
 /*jshint node: true */
-/* globals __base */
-var PgQuery = require(__base + 'models/query/pgquery.js'); 
+/* globals __dbpool */
 
 class PgModel{
-    static _find(tableName, id, instanciation){
-        var dbQuery = new PgQuery('SELECT * FROM ' + tableName + ' WHERE id = $1;',[id]);
-        dbQuery.on('result', instanciation); 
-        dbQuery.perform();
+    static makeFromRow(result){
+        return new Promise( (resolve) => {
+            var res = null;
+            if (rows.length === 0) return __log.error('no record matching');
+            var models = []; 
+            var model = null
+            for(let row in rows){
+                model = new this();
+                for (var key in row) model[key] = row[key];
+                models.push(model);
+            }
+            if(models.length == 1)
+                resolve(models[0]);
+            else
+                resolve(models); 
+            });
     }
-    save(callback){
-        var dbQuery,
+    static find(tableName, id){
+        return __dbpool.query('SELECT * FROM ' + this.tableName + ' WHERE id = $1;',[id]) 
+            .then(this.makeFromRow(result.rows))
+            .catch( (e) => { winston.error(e) }); 
+    }
+    save(){
+        var query,
             inlineFields = this.inlineFields();
         inlineFields[0] += ', updated_at';
         inlineFields[1] += ', localtimestamp';
         if(this.id)
-            dbQuery = new PgQuery('UPDATE '+this.tableName+' SET ('+inlineFields[0]+') = ('+inlineFields[1]+') WHERE id = '+this.id+' RETURNING *;'); 
+            query =  __dbpool.query('UPDATE '+this.tableName+' SET ('+inlineFields[0]+') = ('+inlineFields[1]+') WHERE id = '+this.id+' RETURNING *;'); 
+
         else{
             inlineFields[0] += ', created_at';
             inlineFields[1] += ', localtimestamp';
-            dbQuery = new PgQuery('INSERT INTO '+this.tableName+' ('+inlineFields[0]+') VALUES('+inlineFields[1]+') RETURNING *;');
+            query = __dbpool.query('INSERT INTO '+this.tableName+' ('+inlineFields[0]+') VALUES('+inlineFields[1]+') RETURNING *;');
         }
-        dbQuery.on('result', callback);
-        dbQuery.perform();
+        return query
+            .then(this.makeFromRow(result.rows))
+            .catch( (e) => { winston.error(e) }); 
     }
     delete(callback){
-        var dbQuery = new PgQuery('DELETE FROM '+this.tableName+' WHERE id = $1;',[this.id]);
-        dbQuery.on('result', callback);
-        dbQuery.perform();
-    }
-    initFromRow(row){
-        if (row.length === 0) return console.error('no user matching');
-        if (row.length > 1) return console.error('multiple users matching');
-	    row = row[0];
-        for (var key in row) this[key] = row[key];
+        return __dbpool.query('DELETE FROM '+this.tableName+' WHERE id = $1;',[this.id])
+            .then(this.makeFromRow(result.rows))
+            .catch( (e) => { winston.error(e) });
     }
     inlineFields(){
         var result = [], 
             keys = '', 
             values = '';
-        var safe_keys = new Set(['id','tableName', 'updated_at', 'created_at']);
+        var safe_keys = this.constructor.safe_keys;
+        console.log(safe_keys)
         for(var key in this){
             if((this[key]) && (this[key].constructor.name == 'Function')) continue;
             if(safe_keys.has(key)) continue;
@@ -60,5 +73,7 @@ class PgModel{
         return result; 
     }  
 }
+PgModel.tableName = '<dummy>';
+PgModel.safe_keys = new Set(['id','tableName', 'updated_at', 'created_at']);
 
 module.exports = PgModel;
